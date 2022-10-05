a538d99893148a62a66a8a91cabdecf0315c25f6
EnZooZ <enzooz4889@gmail.com>
2022-10-05 21:07:10 +0700

    CSDL-QLBH

---
+++ Tao-du-lieu.sql
@@ -0,0 +1,255 @@
+﻿-- 1. Tạo dữ liệu
+-- Tạo và sử dụng database
+CREATE DATABASE QLBANHANG2
+GO
+USE QLBANHANG2
+GO
+SET DATEFORMAT DMY
+GO
+-- Tạo các table + Khoá chính
+
+-- Tạo table khách hàng
+CREATE TABLE KHACHHANG (
+ MAKH CHAR(4),
+ HOTEN VARCHAR(40),
+ DCHI VARCHAR(50),
+ SODT VARCHAR(20),
+ NGSINH SMALLDATETIME,
+ NGDK SMALLDATETIME,
+ DOANHSO MONEY,
+ CONSTRAINT PK_KH PRIMARY KEY(MAKH)
+)
+GO
+
+-- Tạo table Nhân viên
+CREATE TABLE NHANVIEN (
+ MANV CHAR(4),
+ HOTEN VARCHAR(40),
+ SODT VARCHAR(20),
+ NGVL SMALLDATETIME,
+ CONSTRAINT PK_NV PRIMARY KEY(MANV)
+)
+GO
+
+-- Tạo table sản phẩm
+CREATE TABLE SANPHAM (
+ MASP CHAR(4),
+ TENSP VARCHAR(40),
+ DVT VARCHAR(20),
+ NUOCSX VARCHAR(40),
+ GIA MONEY,
+ CONSTRAINT PK_SP PRIMARY KEY(MASP)
+)
+GO
+
+-- tạo table Hoá đơn
+CREATE TABLE HOADON (
+ SOHD INT,
+ NGHD SMALLDATETIME,
+ MAKH CHAR(4),
+ MANV CHAR(4),
+ TRIGIA MONEY,
+ CONSTRAINT PK_HD PRIMARY KEY (SOHD)
+)
+GO
+
+-- Taọ table Chi tiết Hoá đơn
+CREATE TABLE CTHD (
+ SOHD INT,
+ MASP CHAR(4),
+ SL INT
+)
+GO
+
+-- Tạo khoá ngoại
+
+-- Tạo khoá ngoại ở bảng hoá đơn
+ALTER TABLE dbo.HOADON
+ ADD CONSTRAINT FK_HD01
+ FOREIGN KEY (MAKH)
+ REFERENCES dbo.KHACHHANG (MAKH)
+ GO
+
+ALTER TABLE dbo.HOADON
+ ADD CONSTRAINT FK_HD02
+ FOREIGN KEY (MANV)
+ REFERENCES dbo.NHANVIEN (MANV)
+ GO
+
+-- Tạo khoá ngoại ở bảng Chi tiết hoá đơn
+ALTER TABLE dbo.CTHD
+ ADD CONSTRAINT FK_CTHD01
+ FOREIGN KEY (SOHD)
+ REFERENCES dbo.HOADON (SOHD)
+ GO
+
+-- NHập Data 
+
+-- NHập data cho bảng Nhân viên
+INSERT INTO dbo.NHANVIEN
+VALUES 
+ ('NV01','Nguyen Nhu Nhut','0927345678','13/04/2006'),
+ ('NV02','Le Thi Phi Yen','0987567390','21/04/2006'),
+ ('NV03','Nguyen Van B','0997047382','27/04/2006'),
+ ('NV04','Ngo Thanh Tuan','0913758498','24/06/2006'),
+ ('NV05','Nguyen Thi Truc Thanh','0918590387','20/07/2006')
+ GO
+
+-- NHập data cho bảng khâchs hàng
+INSERT INTO dbo.KHACHHANG
+VALUES
+ ('KH01','Nguyen Van A','731,Tran Hung Dao, Q 5 ,Tp HCM','08823451','22/10/1960','22/10/2006',13000000),
+ ('KH02','Tran Ngoc Han','23/5, Nguyen Trai, Q 5, Tp HCM','0908256478','03/04/1974','30/07/2006',280000),
+ ('KH03','Tran Ngoc Linh','45, Nguyen Canh Chan, Q 1, Tp HCM','0938776266','12/06/1980','05/08/2006',3860000),
+ ('KH04','Tran Minh Long','50/34 Le Dai Hanh, Q 10, Tp HCM','0917325476','09/03/1965','02/10/2006',250000),
+ ('KH05','Le Nhat Minh','34, Truong Dinh, Q 3, Tp HCM','08246108','10/03/1950','28/10/2006',21000),
+ ('KH06','Le Hoai Thuong','227, Nguyen Van Cu, Q 5, Tp HCM','08631738','31/12/1981','24/11/2006',915000),
+ ('KH07','Nguyen Van Tam','32/3, Tran Binh Trong, Q 5, Tp HCM','0916783565','06/04/1971','01/12/2006',12500),
+ ('KH08','Phan Thi Thanh','45/2, An Duong Vuong, Q 5, Tp HCM','0938435756','10/01/1971','13/12/2006',365000),
+ ('KH09','Le Ha Vinh','873, Le Hong Phong, Q 5, Tp HCM','08654763','03/09/1979','14/01/2007',70000),
+ ('KH10','Ha Duy Lap','34/34B, Nguyen Trai, Q 1, Tp HCM','08768904','02/05/1983','16/01/2007',67500)
+ GO
+
+-- Nhập Data cho bảng Sản phẩm
+INSERT INTO dbo.SANPHAM
+VALUES 
+ ('BC01','But chi','cay','Singapore',3000),
+ ('BC02','But chi','cay','Singapore',5000),
+ ('BC03','But chi','cay','Viet Nam',3500),
+ ('BC04','But chi','hop','Viet Nam',30000),
+ ('BB01','But bi','cay','Viet Nam',5000),
+ ('BB02','But bi','cay','Trung Quoc',7000),
+ ('BB03','But bi','hop','Thai Lan',100000),
+ ('TV01','Tap 100 giay mong','quyen','Trung Quoc',2500),
+ ('TV02','Tap 200 giay mong','quyen','Trung Quoc',4500),
+ ('TV03','Tap 100 giay tot','quyen','Viet Nam',3000),
+ ('TV04','Tap 200 giay tot','quyen','Viet Nam',5500),
+ ('TV05','Tap 100 trang','chuc','Viet Nam',23000),
+ ('TV06','Tap 200 trang','chuc','Viet Nam',53000),
+ ('TV07','Tap 100 trang','chuc','Trung Quoc',34000),
+ ('ST01','So tay 500 trang','quyen','Trung Quoc',40000),
+ ('ST02','So tay loai 1','quyen','Viet Nam',55000),
+ ('ST03','So tay loai 2','quyen','Viet Nam',51000),
+ ('ST04','So tay ','quyen','Thai Lan',55000),
+ ('ST05','So tay mong','quyen','Thai Lan',20000),
+ ('ST06','Phan viet bang','hop','Viet Nam',5000),
+ ('ST07','Phan khong bui','hop','Viet Nam',7000),
+ ('ST08','Bong bang','cai','Viet Nam',1000),
+ ('ST09','But long','cay','Viet Nam',5000),
+ ('ST10','But long','cay','Trung Quoc',7000)
+ GO
+
+-- NHập Data cho bảng Hoá đơn
+
+insert into HOADON values (1001,'23/07/2006','KH01','NV01',320000)
+insert into HOADON values (1002,'12/08/2006','KH01','NV02',840000)
+insert into HOADON values (1003,'23/08/2006','KH02','NV01',100000)
+insert into HOADON values (1004,'01/09/2006','KH02','NV01',180000)
+insert into HOADON values (1005,'20/10/2006','KH01','NV02',3800000)
+insert into HOADON values (1006,'16/10/2006','KH01','NV03',2430000)
+insert into HOADON values (1007,'28/10/2006','KH03','NV03',510000)
+insert into HOADON values (1008,'28/10/2006','KH01','NV03',440000)
+insert into HOADON values (1009,'28/10/2006','KH03','NV04',200000)
+insert into HOADON values (1010,'01/11/2006','KH01','NV01',5200000)
+insert into HOADON values (1011,'04/11/2006','KH04','NV03',250000)
+insert into HOADON values (1012,'30/11/2006','KH05','NV03',21000)
+insert into HOADON values (1013,'12/12/2006','KH06','NV01',5000)
+insert into HOADON values (1014,'31/12/2006','KH03','NV02',3150000)
+insert into HOADON values (1015,'01/01/2007','KH06','NV01',910000)
+insert into HOADON values (1016,'01/01/2007','KH07','NV02',12500)
+insert into HOADON values (1017,'02/01/2007','KH08','NV03',35000)
+insert into HOADON values (1018,'13/01/2007','KH08','NV03',330000)
+insert into HOADON values (1019,'13/01/2007','KH01','NV03',30000)
+insert into HOADON values (1020,'14/01/2007','KH09','NV04',70000)
+insert into HOADON values (1021,'16/01/2007','KH10','NV03',67500)
+insert into HOADON values (1022,'16/01/2007',null,'NV03',7000)
+insert into HOADON values (1023,'17/01/2007',null,'NV01',330000)
+ GO
+
+-- NHập Data cho bảng Chi tiết hoá đơn
+insert into CTHD values (1001,'TV02',10)
+insert into CTHD values (1001,'ST01',5)
+insert into CTHD values (1001,'BC01',5)
+insert into CTHD values (1001,'BC02',10)
+insert into CTHD values (1001,'ST08',10)
+insert into CTHD values (1002,'BC04',20)
+insert into CTHD values (1002,'BB01',20)
+insert into CTHD values (1002,'BB02',20)
+insert into CTHD values (1003,'BB03',10)
+insert into CTHD values (1004,'TV01',20)
+insert into CTHD values (1004,'TV02',20)
+insert into CTHD values (1004,'TV03',20)
+insert into CTHD values (1004,'TV04',20)
+insert into CTHD values (1005,'TV05',50)
+insert into CTHD values (1005,'TV06',50)
+insert into CTHD values (1006,'TV07',20)
+insert into CTHD values (1006,'ST01',30)
+insert into CTHD values (1006,'ST02',10)
+insert into CTHD values (1007,'ST03',10)
+insert into CTHD values (1008,'ST04',8)
+insert into CTHD values (1009,'ST05',10)
+insert into CTHD values (1010,'TV07',50)
+insert into CTHD values (1010,'ST07',50)
+insert into CTHD values (1010,'ST08',100)
+insert into CTHD values (1010,'ST04',50)
+insert into CTHD values (1010,'TV03',100)
+insert into CTHD values (1011,'ST06',50)
+insert into CTHD values (1012,'ST07',3)
+insert into CTHD values (1013,'ST08',5)
+insert into CTHD values (1014,'BC02',80)
+insert into CTHD values (1014,'BB02',100)
+insert into CTHD values (1014,'BC04',60)
+insert into CTHD values (1014,'BB01',50)
+insert into CTHD values (1015,'BB02',30)
+insert into CTHD values (1015,'BB03',7)
+insert into CTHD values (1016,'TV01',5)
+insert into CTHD values (1017,'TV02',1)
+insert into CTHD values (1017,'TV03',1)
+insert into CTHD values (1017,'TV04',5)
+insert into CTHD values (1018,'ST04',6)
+insert into CTHD values (1019,'ST05',1)
+insert into CTHD values (1019,'ST06',2)
+insert into CTHD values (1020,'ST07',10)
+insert into CTHD values (1021,'ST08',5)
+insert into CTHD values (1021,'TV01',7)
+insert into CTHD values (1021,'TV02',10)
+insert into CTHD values (1022,'ST07',1)
+insert into CTHD values (1023,'ST04',6)
+ GO
+
+ -- 2.Thêm vào thuộc tính GHICHU có kiểu dữ liệu varchar(20) cho quan hệ SANPHAM.
+ALTER TABLE dbo.SANPHAM ADD GHICHU VARCHAR(10)
+GO
+-- 3.Thêm vào thuộc tính LOAIKH có kiểu dữ liệu là tinyint cho quan hệ KHACHHANG.
+ALTER TABLE dbo.KHACHHANG ADD LOAIKH TINYINT
+GO
+-- 4.Cập nhật tên “Nguyễn Văn B” cho dữ liệu Khách Hàng có mã là KH01
+UPDATE dbo.KHACHHANG
+SET HOTEN='Nguyễn Văn B'
+WHERE MAKH='KH01'
+GO
+-- 5.Cập nhật tên “Nguyễn Văn Hoan” cho dữ liệu Khách Hàng có mã là KH09 và năm đăng ký là 2007
+UPDATE dbo.KHACHHANG
+SET HOTEN='Nguyễn Văn Hoan'
+WHERE MAKH='KH09' AND YEAR(NGDK)=2007
+GO
+-- 6.Sửa kiểu dữ liệu của thuộc tính GHICHU trong quan hệ SANPHAM thành varchar(100)
+ALTER TABLE dbo.SANPHAM
+ ALTER COLUMN GHICHU VARCHAR(100)
+GO
+-- 7.Xóa thuộc tính GHICHU trong quan hệ SANPHAM.
+ALTER TABLE dbo.SANPHAM
+ DROP COLUMN GHICHU
+GO
+--8.Xoá tất cả dữ liệu khách hàng có năm sinh 1971
+DELETE FROM dbo.KHACHHANG
+WHERE YEAR(NGSINH) = 1971
+GO
+-- 9.Xoá tất cả dữ liệu khách hàng có năm sinh 1971 và năm đăng ký 2006
+DELETE FROM dbo.KHACHHANG
+WHERE YEAR(NGSINH) = 1971 AND YEAR(NGDK) = 2006
+GO
+-- 10.Xoá tất hoá đơn có mã KH = KH01
+DELETE FROM dbo.HOADON
+WHERE MAKH = 'KH01'
+GO
---
+++ Truy-van.sql
@@ -0,0 +1,279 @@
+﻿-- 1.In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất
+SELECT MASP,TENSP FROM dbo.SANPHAM
+WHERE NUOCSX = 'Trung Quoc'
+GO
+-- 2.In ra danh sách các sản phẩm (MASP, TENSP) có đơn vị tính là “cay”, ”quyen”.
+SELECT MASP,TENSP FROM dbo.SANPHAM
+WHERE DVT IN ('cay','quyen')
+GO
+-- 3.In ra danh sách các sản phẩm (MASP,TENSP) có mã sản phẩm bắt đầu là “B” và kết thúc là “01”.
+SELECT MASP,TENSP FROM dbo.SANPHAM
+WHERE MASP LIKE 'B%01'
+GO
+-- 4.In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quốc” sản xuất có giá từ 30.000 đến 40.000.
+SELECT MASP,TENSP FROM dbo.SANPHAM
+WHERE NUOCSX = 'Trung Quoc'AND GIA BETWEEN 30000 AND 40000
+GO
+-- 5.In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” hoặc “Thai Lan” sản xuất có giá từ 30.000 đến 40.000.
+SELECT MASP,TENSP FROM dbo.SANPHAM
+WHERE NUOCSX = 'Trung Quoc'OR NUOCSX = 'Thai Lan'
+AND GIA BETWEEN 30000 AND 40000
+GO
+-- 6.In ra các số hóa đơn, trị giá hóa đơn bán ra trong ngày 1/1/2007 và ngày 2/1/2007
+SELECT SOHD,TRIGIA FROM dbo.HOADON
+WHERE NGHD IN ('01/01/2007','02/01/2007')
+GO
+-- 7.In ra các số hóa đơn, trị giá hóa đơn trong tháng 1/2007, sắp xếp theo ngày (tăng dần) và trị giá của hóa đơn (giảm dần).
+SELECT SOHD,TRIGIA FROM dbo.HOADON
+WHERE YEAR(NGHD) = 2007 AND MONTH(NGHD) = 01
+ORDER BY TRIGIA DESC,NGHD ASC
+GO
+-- 8.In ra danh sách các khách hàng (MAKH, HOTEN) đã mua hàng trong ngày 1/1/2007
+SELECT A.MAKH , A.HOTEN 
+FROM KHACHHANG A, HOADON B
+WHERE A.MAKH = B.MAKH AND NGHD = '1/1/2007'
+GO
+
+SELECT KH.MAKH , KH.HOTEN
+FROM dbo.KHACHHANG AS KH
+ INNER JOIN dbo.HOADON AS HD ON HD.MAKH = KH.MAKH
+WHERE HD.NGHD = '1/1/2007'
+GO
+-- 9.In ra số hóa đơn, trị giá các hóa đơn do nhân viên có tên “Nguyen Van B” lập trong ngày 28/10/2006
+SELECT HD.SOHD,HD.TRIGIA
+FROM dbo.HOADON AS HD
+ INNER JOIN dbo.NHANVIEN AS NV ON NV.MANV = HD.MANV
+WHERE NV.HOTEN = 'Nguyen Van B' AND HD.NGHD='10/28/2006'
+GO
+-- 10.In ra danh sách các sản phẩm (MASP,TENSP) được khách hàng có tên “Nguyen Van A” mua trong tháng 10/2006.
+SELECT SP.MASP,SP.TENSP 
+FROM dbo.KHACHHANG AS KH
+ INNER JOIN dbo.HOADON AS HD ON HD.MAKH = KH.MAKH
+ INNER JOIN dbo.CTHD ON CTHD.SOHD = HD.SOHD
+ INNER JOIN dbo.SANPHAM AS SP ON SP.MASP = CTHD.MASP
+WHERE KH.HOTEN='Nguyen Van A' AND YEAR(HD.NGHD)=2006 AND MONTH(HD.NGHD)=10
+GO
+
+SELECT SP.MASP , SP.TENSP
+FROM (dbo.KHACHHANG AS KH JOIN dbo.HOADON AS HD ON KH.MAKH = HD.MAKH) JOIN ( dbo.SANPHAM AS SP JOIN CTHD ON SP.MASP = CTHD.MASP ) ON HD.SOHD = CTHD.SOHD
+WHERE KH.HOTEN = 'Nguyen Van A' AND MONTH(HD.NGHD)= 10 AND YEAR(HD.NGHD) = 2006
+GO
+-- 11.Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”
+SELECT DISTINCT SOHD FROM dbo.CTHD
+WHERE MASP = 'BB01' OR MASP = 'BB02'
+GO
+-- 12.Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”, mỗi sản phẩm mua với số lượng từ 10 đến 20.
+SELECT DISTINCT SOHD FROM dbo.CTHD
+WHERE (MASP = 'BB01' OR MASP = 'BB02')
+AND SL BETWEEN 10 AND 20
+GO
+-- 13.Tìm các số hóa đơn mua cùng lúc 2 sản phẩm có mã số “BB01” và “BB02”, mỗi sản phẩm mua với số lượng từ 10 đến 20.
+SELECT SOHD
+FROM dbo.CTHD A
+WHERE MASP = 'BB02' AND A.SL BETWEEN 10 AND 20 AND EXISTS (
+	SELECT SOHD 
+	FROM dbo.CTHD B
+	WHERE MASP = 'BB01' AND A.SOHD = B.SOHD AND B.SL BETWEEN 10 AND 20
+	)
+GO
+-- 14.In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất hoặc các sản phẩm được bán ra trong ngày 1/1/2007
+SELECT MASP,TENSP FROM dbo.SANPHAM
+WHERE NUOCSX = 'Trung Quoc'
+UNION
+SELECT SP.MASP,SP.TENSP FROM dbo.SANPHAM AS SP
+ INNER JOIN dbo.CTHD ON CTHD.MASP = SP.MASP
+ INNER JOIN dbo.HOADON AS HD ON HD.SOHD = CTHD.SOHD
+WHERE HD.NGHD = '1/1/2007'
+GO
+-- 15.In ra danh sách các sản phẩm (MASP,TENSP) không bán được.
+SELECT SP.MASP,SP.TENSP FROM dbo.SANPHAM AS SP
+WHERE NOT EXISTS (
+ SELECT MASP FROM dbo.CTHD
+ WHERE CTHD.MASP = SP.MASP
+)
+GO
+-- 16.In ra danh sách các sản phẩm (MASP,TENSP) không bán được trong năm 2006
+SELECT SP.MASP,SP.TENSP FROM dbo.SANPHAM AS SP
+WHERE NOT EXISTS (
+ SELECT MASP 
+ FROM dbo.CTHD
+  INNER JOIN dbo.HOADON AS HD ON HD.SOHD = CTHD.SOHD
+ WHERE CTHD.MASP = SP.MASP AND YEAR(HD.NGHD)=2006
+)
+GO
+-- 17.In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất không bán được trong năm 2006
+SELECT SP.MASP,SP.TENSP FROM dbo.SANPHAM AS SP
+WHERE SP.NUOCSX='Trung Quoc'AND NOT EXISTS (
+ SELECT CTHD.MASP 
+ FROM dbo.CTHD
+  INNER JOIN dbo.HOADON AS HD ON HD.SOHD = CTHD.SOHD
+ WHERE CTHD.MASP = SP.MASP AND YEAR(HD.NGHD)=2006 
+)
+GO
+-- 18.Tìm số hóa đơn đã mua tất cả các sản phẩm do Singapore sản xuất.
+SELECT SOHD FROM dbo.HOADON AS HD
+WHERE NOT EXISTS (
+ SELECT SP.MASP FROM dbo.SANPHAM AS SP
+ WHERE SP.NUOCSX = 'Singapore' AND NOT EXISTS (
+  SELECT SOHD FROM dbo.CTHD
+  WHERE CTHD.SOHD = HD.SOHD AND CTHD.MASP = SP.MASP
+ )
+)
+GO
+-- 19.Có bao nhiêu hóa đơn không phải của khách hàng đăng ký thành viên mua?
+SELECT COUNT(*) AS SOHOADONKHONGPHAIDOTHANHVIENMUA
+FROM dbo.HOADON
+WHERE MAKH IS NULL
+GO
+-- 20.Có bao nhiêu sản phẩm khác nhau được bán ra trong năm 2006.
+SELECT COUNT(DISTINCT CTHD.MASP)
+FROM dbo.HOADON AS HD
+ INNER JOIN dbo.CTHD ON CTHD.SOHD = HD.SOHD
+WHERE YEAR(HD.NGHD) = 2006
+GO
+-- 21.Cho biết trị giá hóa đơn cao nhất, thấp nhất là bao nhiêu ?
+SELECT MAX(HD.TRIGIA) AS TRIGIACCAONHAT, MIN(HD.TRIGIA) AS TRIGIATHAPNHAT
+FROM dbo.HOADON AS HD
+GO
+-- 22.Trị giá trung bình của tất cả các hóa đơn được bán ra trong năm 2006 là bao nhiêu?
+SELECT AVG(HD.TRIGIA) AS TRIGIATRUNGBINH
+FROM dbo.HOADON AS HD
+WHERE YEAR(HD.NGHD) = 2006
+GO
+-- 23.Tính doanh thu bán hàng trong năm 2006
+SELECT SUM(HD.TRIGIA) AS DOANHTHU
+FROM dbo.HOADON AS HD
+WHERE YEAR(HD.NGHD) = 2006
+GO
+-- 24.Tìm số hóa đơn có trị giá cao nhất trong năm 2006.
+SELECT SOHD
+FROM dbo.HOADON
+WHERE TRIGIA= (SELECT MAX(TRIGIA) FROM dbo.HOADON WHERE YEAR(NGHD) =2006 )
+GO
+-- 25.Tìm họ tên khách hàng đã mua hóa đơn có trị giá cao nhất trong năm 2006
+SELECT KH.HOTEN 
+FROM dbo.HOADON AS HD
+ INNER JOIN dbo.KHACHHANG AS KH ON KH.MAKH = HD.MAKH
+WHERE HD.TRIGIA = (SELECT MAX(TRIGIA) FROM dbo.HOADON WHERE YEAR(NGHD) =2006 )
+GO
+-- 26.In ra danh sách 3 khách hàng (MAKH, HOTEN) có doanh số cao nhất.
+SELECT TOP (3) MAKH,HOTEN FROM dbo.KHACHHANG 
+ORDER BY DOANHSO DESC
+GO
+-- 27.In ra danh sách các sản phẩm (MASP, TENSP) có giá bán bằng 1 trong 3 mức giá cao nhất
+SELECT MASP,TENSP FROM dbo.SANPHAM
+WHERE GIA IN (
+ SELECT TOP (3) GIA FROM dbo.SANPHAM
+ ORDER BY GIA DESC
+)
+GO
+-- 28.In ra danh sách các sản phẩm (MASP, TENSP) do “Thai Lan” sản xuất có giá bằng 1 trong 3 mức giá cao nhất (của tất cả các sản phẩm).
+SELECT MASP,TENSP FROM dbo.SANPHAM
+WHERE NUOCSX = 'Thai Lan' 
+AND GIA IN (
+ SELECT TOP (3) GIA FROM dbo.SANPHAM
+ ORDER BY GIA DESC
+)
+GO
+-- 29.In ra danh sách các sản phẩm (MASP, TENSP) do “Trung Quoc” sản xuất có giá bằng 1 trong 3 mức giá cao nhất (của sản phẩm do “Trung Quoc” sản xuất)
+SELECT MASP,TENSP FROM dbo.SANPHAM
+WHERE NUOCSX = 'Trung Quoc' 
+AND GIA IN (
+ SELECT TOP (3) GIA FROM dbo.SANPHAM
+ WHERE NUOCSX = 'Trung Quoc'
+ ORDER BY GIA DESC
+)
+GO
+-- 30. In ra danh sách 3 khách hàng có doanh số cao nhất (sắp xếp theo kiểu xếp hạng).
+SELECT TOP (3) MAKH ,HOTEN FROM dbo.KHACHHANG
+ORDER BY DOANHSO DESC
+GO
+-- 31.Tính tổng số sản phẩm do “Trung Quoc” sản xuất.
+SELECT COUNT(*) AS SOSP FROM dbo.SANPHAM
+WHERE NUOCSX = 'Trung Quoc'
+GO
+-- 32.Tính tổng số sản phẩm của từng nước sản xuất.
+SELECT NUOCSX , COUNT(*) AS SOSP 
+FROM dbo.SANPHAM
+GROUP BY NUOCSX
+GO
+-- 33.Với từng nước sản xuất, tìm giá bán cao nhất, thấp nhất, trung bình của các sản phẩm.
+SELECT NUOCSX , MAX(GIA) AS GIACAONHAT , MIN(GIA) AS GIATHAPNHAT , AVG(GIA) AS GIATB
+FROM dbo.SANPHAM
+GROUP BY NUOCSX
+GO
+-- 34.Tính doanh thu bán hàng mỗi ngày.
+SELECT CONVERT(VARCHAR(20),NGHD,103) AS NGAY , SUM(TRIGIA) AS DOANHTHU 
+FROM dbo.HOADON
+GROUP BY NGHD
+ORDER BY NGHD ASC
+GO
+-- 35.Tính tổng số lượng của từng sản phẩm bán ra trong tháng 10/2006.
+SELECT CTHD.MASP , SUM(SL) AS SL
+FROM dbo.HOADON AS HD
+ INNER JOIN dbo.CTHD ON CTHD.SOHD = HD.SOHD
+WHERE MONTH(HD.NGHD) = 10 AND YEAR(HD.NGHD) = 2006
+GROUP BY CTHD.MASP
+GO
+-- 36.Tính doanh thu bán hàng của từng tháng trong năm 2006
+SELECT MONTH(NGHD) AS THANG , SUM(TRIGIA) AS TONGDOANHTHU
+FROM dbo.HOADON
+WHERE YEAR(NGHD) = 2006
+GROUP BY MONTH(NGHD)
+GO
+-- 37.Tìm hóa đơn có mua ít nhất 4 sản phẩm khác nhau
+SELECT SOHD FROM dbo.CTHD
+GROUP BY SOHD
+HAVING COUNT(DISTINCT MASP) > 3
+GO
+-- 38.Tìm hóa đơn có mua 3 sản phẩm do “Viet Nam” sản xuất (3 sản phẩm khác nhau).
+SELECT CTHD.SOHD 
+FROM dbo.CTHD 
+ JOIN dbo.SANPHAM AS SP ON CTHD.MASP = SP.MASP
+WHERE SP.NUOCSX ='Viet Nam'
+GROUP BY CTHD.SOHD
+HAVING COUNT(DISTINCT CTHD.MASP) = 3
+GO
+-- 39.Tìm khách hàng (MAKH, HOTEN) có số lần mua hàng nhiều nhất.
+SELECT TOP (1) KH.MAKH,KH.HOTEN
+FROM dbo.HOADON AS HD 
+ INNER JOIN dbo.KHACHHANG AS KH ON KH.MAKH = HD.MAKH
+GROUP BY KH.MAKH , KH.HOTEN
+ORDER BY COUNT(*) DESC
+GO
+-- 40.Tháng mấy trong năm 2006, doanh số bán hàng cao nhất ?
+SELECT TOP (1) MONTH(NGHD) AS THANG FROM dbo.HOADON
+WHERE YEAR(NGHD) = 2006
+GROUP BY MONTH(NGHD)
+ORDER BY SUM(TRIGIA) DESC
+GO
+-- 41.Tìm sản phẩm (MASP, TENSP) có tổng số lượng bán ra thấp nhất trong năm 2006.
+SELECT TOP (1) SP.MASP , SP.TENSP
+FROM dbo.HOADON AS HD
+ INNER JOIN dbo.CTHD ON CTHD.SOHD = HD.SOHD
+ INNER JOIN dbo.SANPHAM AS SP ON SP.MASP = CTHD.MASP
+WHERE YEAR(HD.NGHD)= 2006
+GROUP BY SP.MASP, SP.TENSP
+ORDER BY SUM(CTHD.SL) ASC
+GO
+-- 42.*Mỗi nước sản xuất, tìm sản phẩm (MASP,TENSP) có giá bán cao nhất.
+SELECT NUOCSX,MASP,TENSP FROM dbo.SANPHAM
+GROUP BY NUOCSX,MASP,TENSP
+ORDER BY MAX(GIA) DESC
+GO
+-- 43.Tìm nước sản xuất sản xuất ít nhất 3 sản phẩm có giá bán khác nhau.
+SELECT NUOCSX FROM dbo.SANPHAM
+GROUP BY NUOCSX
+HAVING COUNT(DISTINCT GIA) >2
+GO
+-- 44.Trong 10 khách hàng có doanh số cao nhất, tìm khách hàng có số lần mua hàng nhiều nhất.
+SELECT TOP (1) MAKH ,HOTEN 
+FROM (
+		SELECT TOP (10) KH.MAKH , KH.HOTEN , COUNT(HD.MAKH) AS SL
+		FROM  dbo.KHACHHANG AS KH 
+		 JOIN dbo.HOADON AS HD ON KH.MAKH = HD.MAKH
+		GROUP BY KH.MAKH , KH.DOANHSO , KH.HOTEN
+		ORDER BY KH.DOANHSO DESC
+		) KH
+ORDER BY SL DESC
+GO
